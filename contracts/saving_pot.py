b6bb46ccd16aa7dc9c6af9b50f4c79d162d3922c
dingxin <wdx7266@vip.qq.com>
2018-11-12 14:08:37 +0800

    update contract

--- contracts/saving_pot.py
+++ contracts/saving_pot.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from boa.interop.System.Runtime import CheckWitness
+from boa.interop.Ontology.Native import Invoke
+from boa.builtins import ToScriptHash, concat, state
+from boa.interop.System.Storage import GetContext, Get, Put, Delete
+from boa.interop.System.Runtime import CheckWitness, GetTime, Notify
+from boa.interop.System.ExecutionEngine import GetExecutingScriptHash
+
+ont_address = bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01')
+ong_address = bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02')
+saving_pot_address = GetExecutingScriptHash()
+
+CTX = GetContext()
+SAVING_ONT_TIME_PREFIX = 'STT'
+SAVING_ONT_AMOUNT_PREFIX = 'STA'
+SAVING_ONG_TIME_PREFIX = 'SGT'
+SAVING_ONG_AMOUNT_PREFIX = 'SGA'
+
+
+def main(operation, args):
+    if operation == "saving_ont":
+        return saving_ont(args[0], args[1])
+    elif operation == "saving_ong":
+        return saving_ong(args[0], args[1])
+    elif operation == "create_ont_pot":
+        return create_ont_pot(args[0], args[1])
+    elif operation == "create_ong_pot":
+        return create_ong_pot(args[0], args[1])
+    else:
+        return revert()
 
 
 def revert():
@@ -24,3 +51,80 @@ def require_script_hash(key):
 
 def require_witness(witness):
     require(CheckWitness(witness))
+
+
+def add(a, b):
+    c = a + b
+    require(c >= a)
+    return c
+
+
+def sub(a, b):
+    c = a - b
+    require(a >= c)
+    return c
+
+
+def concat_key(str1, str2):
+    return concat(concat(str1, '_'), str2)
+
+
+def transfer_ont(from_acct, to_acct, amount):
+    require_witness(from_acct)
+    transfer_param = state(from_acct, to_acct, amount)
+    res = Invoke(0, ont_address, 'transfer', [transfer_param])
+    if res and res == b'\x01':
+        return True
+    else:
+        return False
+
+
+def transfer_ong(from_acct, to_acct, amount):
+    require_witness(from_acct)
+    transfer_param = state(from_acct, to_acct, amount)
+    res = Invoke(0, ong_address, 'transfer', [transfer_param])
+    if res and res == b'\x01':
+        return True
+    else:
+        return False
+
+
+def create_ont_pot(from_acct, time_limit):
+    require_witness(from_acct)
+    saving_time_key = concat_key(SAVING_ONT_TIME_PREFIX, from_acct)
+    if not Get(CTX, saving_time_key):
+        Put(CTX, saving_time_key, time_limit)
+        saving_amount_key = concat_key(SAVING_ONT_AMOUNT_PREFIX, from_acct)
+        Put(CTX, saving_amount_key, 0)
+    else:
+        revert()
+
+
+def create_ong_pot(from_acct, time_limit):
+    require_witness(from_acct)
+    saving_time_key = concat_key(SAVING_ONG_TIME_PREFIX, from_acct)
+    if not Get(CTX, saving_time_key):
+        Put(CTX, saving_time_key, time_limit)
+        saving_amount_key = concat_key(SAVING_ONG_AMOUNT_PREFIX, from_acct)
+        Put(CTX, saving_amount_key, 0)
+    else:
+        revert()
+
+
+def saving_ont(from_acct, amount):
+    saving_time_key = concat_key(SAVING_ONT_TIME_PREFIX, from_acct)
+    saving_time = Get(CTX, saving_time_key)
+    if saving_time <= GetTime():
+        transfer_ont(from_acct, saving_pot_address, amount)
+    else:
+        revert()
+
+
+def saving_ong(from_acct, amount):
+    saving_time_key = concat_key(SAVING_ONG_TIME_PREFIX, from_acct)
+    saving_time = Get(CTX, saving_time_key)
+    Notify([saving_time, GetTime()])
+    if saving_time <= GetTime():
+        transfer_ong(from_acct, saving_pot_address, amount)
+    else:
+        revert()
