d12b87622ee7deb388e7e9581fc2fa6d6394ac2d
dingxin <wdx7266@vip.qq.com>
2018-11-15 23:49:17 +0800

    add unit test

--- test/test_invoke_saving_pot.py
+++ test/test_invoke_saving_pot.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import time
 import unittest
+from unittest.mock import patch
 
 from ontology.ont_sdk import OntologySdk
 from ontology.wallet.wallet_manager import WalletManager
 
 from pot.default_settings import (
-    CONTRACT_ABI,
+    GAS_LIMIT,
+    GAS_PRICE,
     WALLET_PATH,
+    CONTRACT_ABI,
     ONT_RPC_ADDRESS,
     CONTRACT_ADDRESS_HEX
 )
@@ -23,14 +26,16 @@ password = input('password: ')
 gas_limit = 20000000
 gas_price = 500
 acct = wallet_manager.get_account('AKeDu9QW6hfAhwpvCwNNwkEQt1LkUQpBpW', password)
-ont_id_acct = wallet_manager.get_account('did:ont:AHBB3LQNpqXjCLathy7vTNgmQ1cGSj8S9Z', password)
-saving_pot = InvokeSavingPot(CONTRACT_ABI, CONTRACT_ADDRESS_HEX)
+saving_pot = InvokeSavingPot(ontology, CONTRACT_ABI, CONTRACT_ADDRESS_HEX)
 
 
-class TestSmartContract(unittest.TestCase):
+class TestInvokeSavingPot(unittest.TestCase):
     def test_create_ont_pot(self):
         time_limit = 60
-        saving_pot.create_ong_pot(acct, time_limit)
+        tx_hash = saving_pot.create_ong_pot(acct, time_limit, GAS_LIMIT, GAS_PRICE)
+        time.sleep(6)
+        event = ontology.rpc.get_smart_contract_event_by_tx_hash(tx_hash)
+        print(event)
 
 
 if __name__ == '__main__':
